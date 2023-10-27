467ceb92b9a5171e4edb925a26e5853ed7ad625f
Ishan <130207814+Ishan-Pal@users.noreply.github.com>
2023-10-27 17:05:38 +0530

    Time: 11 ms (26.68%), Space: 43.6 MB (93.54%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,19 @@
 class Solution {
-  public int singleNumber(int[] nums) {
-    int res = 0;
-    for (int x : nums) {
-      res ^= x;
+    public int singleNumber(int[] nums) {
+        Set<Integer> arr=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!arr.contains(nums[i]))
+            {
+                arr.add(nums[i]);
+            }
+            else
+            {
+                arr.remove(nums[i]);
+            }
+        }
+        return (int)arr.iterator().next();
+        
+        
     }
-    return res;
-  }
 }
